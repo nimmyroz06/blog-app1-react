5149cd595a650278cd8949c96df3d46e3c662e38
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-07-29 00:23:46 +0530

    created

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,6 +1,41 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const CreatePost = () => {
+const [token,setToken]=useState(sessionStorage.getItem("token"))
+
+
+    const [input,setInput]=useState(
+        {"message":"","UserId":sessionStorage.getItem("UserId")}
+    )
+
+
+    const inputHandler=(event)=>{
+        setInput({...input,[event.target.name]:event.target.value})
+    }
+
+
+const readValue=()=>{
+    console.log(input)
+    axios.post("",input,{
+        headers:{"token":token,"Content-Type":"application/json"}
+    }).then(
+        (response)=>{
+            if (response.data.status=="success") {
+                alert("Posted succesfully")
+                
+            } else {
+                alert("something went wrong!!")
+            }
+
+        }
+    ).catch(
+        (error)=>{
+            console.log(error)
+        }
+    )
+}
+
   return (
     <div>
         <div className="container">
@@ -9,10 +44,10 @@ const CreatePost = () => {
                     <div className="row">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Post a Message</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea name="" value={input.message} className="form-control" onChange={inputHandler}></textarea>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-success">Post</button>
+                            <button onClick={readValue} className="btn btn-success">Post</button>
                         </div>
                     </div>
                 </div>
