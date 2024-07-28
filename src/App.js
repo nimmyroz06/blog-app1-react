403afad5cea5425cefc2833d6ad8ac8aaa349004
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-07-28 19:38:39 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <SignUp/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<SignIn/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12">
+                            <label htmlFor="" className="form-label">Email Id</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12">
+                        <label htmlFor="" className="form-label">Password</label>
+                        <input type="password" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12">
+                            <button className="btn btn-success">SingIn</button>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12">
+                            <a href="" className="btn btn-secondary">New Users Click Here</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SignIn
