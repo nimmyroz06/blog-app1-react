559853491cdf6c14e7e442a3c401d9c4cb15ffe7
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-07-28 19:58:56 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import { BrowserRouter, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <SignUp/>
-    </div>
+    <BrowserRouter>
+    <Router>
+      <Routes path='/' element={<SignIn/>}/>
+      <Routes path='/signup' element={<SignUp/>}/>
+    </Router>
+    </BrowserRouter>
   );
 }
 
