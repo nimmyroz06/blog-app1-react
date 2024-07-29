13ee3a114f46e67007dc2b1a28f41d2e3753f54a
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-07-29 21:26:28 +0530

    updated

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,99 +1,95 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 
-const SignUp = () => {
-    const [input, setInput] = new useState(
-        {
-            "name": "", "dob": "", "gender": "", "email": "", "password": "", "cnfpass": ""
-        }
-    )
-    const inputHandler = (event) => {
-        setInput({ ...input, [event.target.name]: event.target.value })
+const Signup = () => {
+
+    const [input,setInput]=new useState(
+    {   "name":"",
+        "phoneno":"",
+        "email":"",
+        "password":"",
+        "confirmpass":""
     }
-    const readValue = () => {
-        if (input.password == input.cnfpass) {
-
-
-            let newInput = { "name": input.name, "dob": input.dob, "gender": input.gender, "email": input.email, "password": input.password }
-            axios.post("", newInput).then(
-                (response) => {
+    )
+    const inputHandler=(event)=>{
+        setInput({...input,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        if (input.password==input.confirmpass) {
+            let newInput={"name":input.name,
+                    "phoneno":input.phoneno,
+                    "email":input.email,
+                    "password":input.password}
+            axios.post("http://localhost:3030/signUp",newInput).then(
+                (response)=>{
                     console.log(response.data)
-                    if (response.data.status=="success") {
-                        alert("Successfully registered")
-                        setInput({
-                            "name": "", "dob": "", "gender": "", "email": "", "password": "", "cnfpass": ""
-                        })
-                        
+                    if (response.data.status=="Success") {
+                            alert("Registered successfully")
+                            setInput({ "name":"",
+                                "phoneno":"",
+                                "email":"",
+                                "password":"",
+                                "confirmpass":""})
                     } else {
-                        alert("Email id already exist")
-                        setInput({
-                            "name": "", "dob": "", "gender": "", "email": "", "password": "", "cnfpass": ""
-                        })
+                        alert("Email id already exists")
+                        setInput({ "name":"",
+                            "phoneno":"",
+                            "email":"",
+                            "password":"",
+                            "confirmpass":""})
                     }
-
-
                 }
-
             ).catch(
-                (error) => {
+                (error)=>{
                     console.log(error)
                 }
             )
-
         } else {
-            alert("Password and confirm password doesn't match")
+          alert("Password doesn't match")  
         }
+
     }
-    return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <div className="row g-3">
-                            <div className="col col-12 col-sm-12 md-col-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">Name</label>
-                                <input type="text" className="form-control" name='name' value={input.name} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 md-col-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">Date Of Birth</label>
-                                <input type="date" name="dob" id="" className="form-control" value={input.dob} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 md-col-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">Gender</label>
-                                <select name="gender" id="" className="form-control" value={input.gender} onChange={inputHandler}>
-                                    <option value="select">Select</option>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                </select>
-                            </div>
-                            <div className="col col-12 col-sm-12 md-col-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">Email</label>
-                                <input type="text" className="form-control" name='email' value={input.email} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 md-col-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">password</label>
-                                <input type="password" className="form-control" name='password' value={input.password} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 md-col-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">Confirm password</label>
-                                <input type="password" className="form-control" name='cnfpass' value={input.cnfpass} onChange={inputHandler} />
-                            </div>
-                            <div className="col col-12 col-sm-12 md-col-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button onClick={readValue} className="btn btn-success">Register</button>
-                            </div>
-                            <div className="col col-12 col-sm-12 md-col-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <a href="/" className="btn btn-primary">Login</a>
-                            </div>
+
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 cl-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="from-label">NAME</label>
+                            <input type="text" className="form-control" name='name' value={input.name} onChange={inputHandler} />
                         </div>
-
-
-
-
+                        <div className="col col-12 cl-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">PHONE NO</label>
+                            <input type="text" className="form-control" name='phoneno' value={input.phoneno} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 cl-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">EMAIL ID</label>
+                            <input type="email" className="form-control" name='email' value={input.email} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 cl-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label" >
+                                PASSWORD
+                            </label>
+                            <input type="password" className="form-control" name='password' value={input.password} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 cl-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">CONFRIM PASSWORD</label>
+                                <input type="password" className="form-control"  name='confirmpass' value={input.confirmpass} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 cl-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success" onClick={readValue}>REGISTER</button>
+                        </div>
+                        <div className="col col-12 cl-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                           <a href="/" className="btn btn-primary">BACK TO LOGIN</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    </div>
+  )
 }
 
-export default SignUp
+export default Signup
