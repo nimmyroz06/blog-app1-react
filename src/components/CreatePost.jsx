22d5981be86caac80732d996c730bd374de73ac3
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-07-29 21:31:26 +0530

    updated

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,59 +1,52 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 
-const CreatePost = () => {
-const [token,setToken]=useState(sessionStorage.getItem("token"))
-
+const Createpost = () => {
 
     const [input,setInput]=useState(
-        {"message":"","UserId":sessionStorage.getItem("UserId")}
+        {
+            "userId":sessionStorage.getItem("userId"),
+            "Message":""
+        }
     )
-
-
     const inputHandler=(event)=>{
         setInput({...input,[event.target.name]:event.target.value})
     }
-
-
-const readValue=()=>{
-    console.log(input)
-    axios.post("",input,{
-        headers:{"token":token,"Content-Type":"application/json"}
-    }).then(
-        (response)=>{
-            if (response.data.status=="success") {
-                alert("Posted succesfully")
-                
-            } else {
-                alert("something went wrong!!")
+    const readValue=()=>{
+        console.log(input)
+        axios.post("http://localhost:3030/create",input,{headers:{"token":sessionStorage.getItem("token"),"Content-Type":"application/json"}}).then(
+            (response)=>{
+                if (response.data.status=="success") {
+                    alert("Posted successfully")
+                } else {
+                    alert("Something went wrong")
+                }
             }
-
-        }
-    ).catch(
-        (error)=>{
-            console.log(error)
-        }
-    )
-}
-
+        ).catch(
+            (error)=>{
+                console.log(error)
+            }
+        )
+    }
   return (
     <div>
-        <div className="container">
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row">
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <label htmlFor="" className="form-label">Post a Message</label>
-                            <textarea name="" value={input.message} className="form-control" onChange={inputHandler}></textarea>
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button onClick={readValue} className="btn btn-success">Post</button>
-                        </div>
+    <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-2">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">POST A MESSAGE</label>
+                        <textarea name="Message" value={input.Message} onChange={inputHandler} id="" className="form-control"></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success" onClick={readValue}>POST</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    </div>
   )
 }
-export default CreatePost
+
+export default Createpost
